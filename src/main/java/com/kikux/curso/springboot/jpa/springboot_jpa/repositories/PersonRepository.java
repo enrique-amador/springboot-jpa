1f358213f87fe9b80bcaec168c8ee0c23db5bcce
kikux-ubuntu <i.am.kike@gmail.com>
2025-05-13 14:06:55 -0600

    implementing buscarByProgrammingLanguage

--- src/main/java/com/kikux/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/kikux/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,9 +1,16 @@
 package com.kikux.curso.springboot.jpa.springboot_jpa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.kikux.curso.springboot.jpa.springboot_jpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
+    List<Person> findByProgrammingLanguage(String programmingLanguage); //this method is implemented automatically
+    
+    @Query("select p from Person p where p.programmingLanguage=?1")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage); //this method cannot be auto implemented so we use @Query
 }
