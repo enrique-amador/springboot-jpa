5a06316457e452afbba78fddfefec6bd0b4edf6b
kikux-ubuntu <i.am.kike@gmail.com>
2025-08-04 12:32:20 -0600

    creando metodos usando Query

--- src/main/java/com/kikux/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/kikux/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,6 +1,8 @@
 package com.kikux.curso.springboot.jpa.springboot_jpa;
 
+
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,11 +25,20 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		getOne();
+		// create();
+		// getLists();
+	}
+
+	public void create() {
+		Person p = new Person(null, "Lalo", "Thor", "Python");
+		Person personNew = repository.save(p);
+		System.out.println(personNew);
+
 	}
 
 	private void getOne() {
 		// Optional<Person> optPerson = repository.findById(1L);
-		// if(optPerson.isPresent()) { we avoid with this an exception
+		// if(optPerson.isPresent()) { we avoid an exception with this
 		// 	System.out.println(optPerson.get());
 		// }
 
@@ -35,9 +46,9 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 		// repository.findById(1L).ifPresent(p -> System.out.println(p));
 		repository.findById(1L).ifPresent(System.out::println);
 		
-		// repository.findOneLikeName("SEFA").ifPresent(System.out::println);
+		repository.findOneLikeName("jos").ifPresent(System.out::println);
 
-		repository.findByNameContaining("jose").ifPresent(System.out::println);
+		// repository.findByNameContaining("jose").ifPresent(System.out::println);
 
 	}
 
@@ -45,9 +56,13 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 		// List<Person> persons = (List<Person>) repository.findAll();
 		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Python", "Pepe"); 
 		persons.stream().forEach(p -> System.out.println(p));
-
+		System.out.println("=====================================");
 		List<Object[]> peopleData = repository.buscarPersonDataPorName("Andres");
 		peopleData.stream().forEach(p -> System.out.println(p[0] + " es experto en: " + p[1]));
+		System.out.println("=====================================");
+		List<Object[]> peopleData2 = repository.buscarPersonData();
+		peopleData2.stream().forEach(p -> System.out.println(p[0] + " es experto en: " + p[1]));
+		
 	}
 
 }
